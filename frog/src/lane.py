1cc0eefc651b2744df8ef7f23ef7dbbd4524a9bb
andre.roberge <andre.roberge@d96c7b2f-723b-0410-8533-71012bf6bf24>
2007-10-26 00:14:53 +0000

    logs with different length added; turtles added.

--- frog/src/lane.py
+++ frog/src/lane.py
@@ -23,7 +23,16 @@ img_filename = os.path.join(DIR, "images", 'car_truck01.png')
 car_images.append(image.load(img_filename))
 
 img_filename = os.path.join(DIR, "images", 'log.png')
-log_image = image.load(img_filename)
+log_images = [image.load(img_filename)]
+img_filename = os.path.join(DIR, "images", 'log_medium.png')
+log_images.append(image.load(img_filename))
+img_filename = os.path.join(DIR, "images", 'log_small.png')
+log_images.append(image.load(img_filename))
+
+turtle_images = []
+for i in range(5):
+    img_filename = os.path.join(DIR, "images", 'turtle%d.png'%i)
+    turtle_images.append(image.load(img_filename))
 
 class LaneObject(object):
     def __init__(self, lane, y_values):
@@ -65,7 +74,8 @@ class Log(LaneObject):
     '''floating log'''
     def __init__(self, lane, x_values, y_values, vx):
         super(Log, self).__init__(lane, y_values)
-        self.image = log_image
+        self.image = log_images[random.randint(0, len(log_images)-1)]
+        self.sunk = False  # a log always floats
         if x_values[lane] == 0:
             self.x = 0 - self.image.width
             self.vx = vx
@@ -75,6 +85,67 @@ class Log(LaneObject):
 
         self.width = self.image.width
         self.height = self.image.height
+
+class Turtle(LaneObject):
+    '''swimming_turtle'''
+    def __init__(self, lane, x_values, y_values, vx, swimming_cycle, cycle_time):
+        super(Turtle, self).__init__(lane, y_values)
+        # the following is a series of floating/diving turtle images
+        self.images = turtle_images
+        self.sunk = False
+        # fully at the surface:
+        self.image = self.images[0]  #
+        if x_values[lane] == 0:
+            self.x = 0 - self.image.width
+            self.vx = vx
+        else:
+            self.x = x_values[lane]
+            self.vx = -vx
+
+        self.width = self.image.width
+        self.height = self.image.height
+
+        #self.swimming_cycle = swimming_cycle
+        self.swimming_cycle = [1.5, 0.3, 0.3, 0.3, 0.4]
+        self.half_cycle_length = len(self.swimming_cycle)
+        r = self.swimming_cycle[:]
+        r.reverse()
+
+        #self.cycle_time = cycle_time
+        self.cycle_time = 0
+        self.half_total_cycle_time = 0
+        s = []
+        for t in self.swimming_cycle:
+            self.half_total_cycle_time += t
+            s.append(self.half_total_cycle_time)
+        self.swimming_cycle = s[:]
+        total = 0
+        for t in r:
+            total += t
+            self.swimming_cycle.append(self.half_total_cycle_time+total)
+
+
+    def update(self, dt):
+        self.cycle_time += dt
+        if self.cycle_time > 2*self.half_total_cycle_time:
+            self.cycle_time = 0
+        self.image = self.images[0]
+        if self.cycle_time < self.half_total_cycle_time:
+            # cycle forward
+            for i in range(self.half_cycle_length-1):
+                if self.cycle_time > self.swimming_cycle[i]:
+                    self.image = self.images[i+1]
+        else:
+            # cycle backward
+            self.image = self.images[-1]
+            for i in range(self.half_cycle_length, 2*self.half_cycle_length):
+                if self.cycle_time > self.swimming_cycle[i]:
+                    self.image = self.images[2*self.half_cycle_length-i-2]
+        if self.image == self.images[-1]:
+            self.sunk = True
+        else:
+            self.sunk = False
+        super(Turtle, self).update(dt)
 
 class LillyPad(object):
     def __init__(self, x, y):
