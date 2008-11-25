d9ce0cef42f85be0213eee9d1658b5fe2d563dfc
andre.roberge <andre.roberge@d96c7b2f-723b-0410-8533-71012bf6bf24>
2008-11-25 15:56:50 +0000

    First commit of django based wiki app

---
+++ django_1/crunchy_wiki/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^crunchy_wiki/', include('crunchy_wiki.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/(.*)', admin.site.root),
+    (r'^crunchy_wiki/(?P<page_name>[^/]+)/$', 'crunchy_wiki.wiki.views.view_page'),
+    (r'^crunchy_wiki/(?P<page_name>[^/]+)/edit/$', 'crunchy_wiki.wiki.views.edit_page'),
+    (r'^crunchy_wiki/(?P<page_name>[^/]+)/save/$', 'crunchy_wiki.wiki.views.save_page'),
+
+)
