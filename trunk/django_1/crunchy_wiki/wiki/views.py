b356ac4895d51b4a60111644a4b0b2a18097b908
andre.roberge <andre.roberge@d96c7b2f-723b-0410-8533-71012bf6bf24>
2008-11-25 15:56:50 +0000

    First commit of django based wiki app

---
+++ trunk/django_1/crunchy_wiki/wiki/views.py
@@ -0,0 +1,31 @@
+# Create your views here.
+from crunchy_wiki.wiki.models import Page
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+
+def view_page(request, page_name):
+    try:
+        page = Page.objects.get(pk=page_name)
+    except Page.DoesNotExist:
+        return render_to_response("create.html", {"page_name": page_name})
+    return render_to_response("view.html", {"page_name": page_name,
+                                            "content": page.content})
+
+def edit_page(request, page_name):
+    try:
+        page = Page.objects.get(pk=page_name)
+        content = page.content
+    except Page.DoesNotExist:
+        content = ''
+    return render_to_response("edit.html", {"page_name": page_name,
+                                                "content": content})
+
+def save_page(request, page_name):
+    content = request.POST["content"]
+    try:
+        page = Page.objects.get(pk=page_name)
+        page.content = content
+    except Page.DoesNotExist:
+        page = Page(name=page_name, content=content)
+    page.save()
+    return HttpResponseRedirect("/crunchy_wiki/%s/" % page_name)
