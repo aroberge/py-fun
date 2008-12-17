03e7f98b831e9c994aef3da57bca5a4c0a423519
andre.roberge <andre.roberge@d96c7b2f-723b-0410-8533-71012bf6bf24>
2008-12-17 22:40:57 +0000

    Completed work in breaking up files - added some comments.

--- plugin-work/many_files/calculator.py
+++ plugin-work/many_files/calculator.py
@@ -3,11 +3,7 @@
 See http://effbot.org/zone/simple-top-down-parsing.txt for detailed explanations
 as to how it works.
 
-This is the main file used to demonstrate plugin frameworks.  In this
-example, we import known objects residing in separate python files explicitly
-specified by name.
-In a true plugin system, we would import arbitrary objects residing in
-arbitrarily named files.
+This is the main file used to demonstrate plugin frameworks.  
 """
 
 import re
