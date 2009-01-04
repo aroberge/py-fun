1bac0004fb743bc3accfc9489732eae79a416b33
andre.roberge <andre.roberge@d96c7b2f-723b-0410-8533-71012bf6bf24>
2009-01-04 20:53:53 +0000

    Reorganizing the code.

---
+++ trunk/image_misc/src/dialogs.py
@@ -0,0 +1,43 @@
+'''Simple dialogs for the main application'''
+
+import tkMessageBox, tkSimpleDialog
+import Tkinter as tk
+
+class ValidatedIntegerDialog(tkSimpleDialog.Dialog):
+    def validate(self):
+        result = self.choice.get()
+        if result == '':
+            return
+        try:
+            self.result = int(result)
+            if self.result < self.min_value:
+                raise
+        except:
+            tkMessageBox.showerror('',
+                        "Integer value greater than %s required."%self.min_value)
+            self.result = None
+        return self.result
+
+class ImageFrequency(ValidatedIntegerDialog):
+    def body(self, parent):
+        tk.Label(parent, text="Select the image drawing frequency").pack()
+        self.choice = tk.Entry(parent)
+        self.choice.pack()
+        self.min_value = 1
+        return self.choice # initial focus
+
+class NumberOfPolygons(ValidatedIntegerDialog):
+    def body(self, parent):
+        tk.Label(parent, text="Select the number of polygons").pack()
+        self.choice = tk.Entry(parent)
+        self.choice.pack()
+        self.min_value = 1
+        return self.choice # initial focus
+
+class NumberOfSides(ValidatedIntegerDialog):
+    def body(self, parent):
+        tk.Label(parent, text="Select the number of sides per polygon").pack()
+        self.choice = tk.Entry(parent)
+        self.choice.pack()
+        self.min_value = 3
+        return self.choice # initial focus
