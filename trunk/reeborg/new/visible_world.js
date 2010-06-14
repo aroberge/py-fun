fa548596f875a1250ea381f9d39619cf48a65883
andre.roberge <andre.roberge@d96c7b2f-723b-0410-8533-71012bf6bf24>
2010-06-14 13:17:04 +0000

    ui improvements of various kinds.

--- trunk/reeborg/new/visible_world.js
+++ trunk/reeborg/new/visible_world.js
@@ -8,6 +8,7 @@ Raphael.fn.button = function () {
     return this.rect(0, 0, 20, 20);
 };
 
+
 function Controls(world) {
     this.edit_button = world.paper.button().attr({x:10, y:0, fill:"red", title:"edit"});
     this.move_button = world.paper.button().attr({x:40, y:0, fill:"green", title:"move"});
@@ -74,8 +75,11 @@ function VisibleRobot(world) {
     this.offset_y = world.world_height - world.margin_bottom - this.height;
     this.style = "classic";
     this.angle = 0;
-    this.x = 0;
-    this.y = 0;
+    this.x = undefined;
+    this.y = undefined;
+    this.path_x = undefined;
+    this.path_y = undefined;
+    this.path_history = undefined;
 
     this.robot = {
         "east": world.paper.image("../images/robot_e.png", 0, 0, this.width, this.height).hide(),
@@ -93,67 +97,112 @@ function VisibleRobot(world) {
             this.x += 11;
             this.y += 13;
             this.current.hide();
-            this.current = this.robot.ellipse.attr({cx:this.x, cy:this.y}).rotate(this.angle, true).show();
+            this.current = this.robot.ellipse.attr({cx:this.x, cy:this.y}).rotate(this.angle, true).show().toFront();
         }
         else{
             this.style = "classic";
             this.x -= 11;
             this.y -= 13;
             this.current.hide();
-            this.current = this.robot[this.current_orientation].attr({x:this.x, y:this.y}).show();
+            this.current = this.robot[this.current_orientation].attr({x:this.x, y:this.y}).show().toFront();
         }
     };
 
     this.teleport = function (column, row) {
         this.x = this.offset_x + (row-1)*world.tile_size;
         this.y = this.offset_y + (column-1)*world.tile_size;
+        this.path_x = this.x + 14;
+        this.path_y = this.y + 17;
+        this.path_history = [[this.path_x, this.path_y]];
         this.row = row;
         this.column = column;
         this.current.attr({x:this.x, y:this.y}).show();
     };
 
-    this.move = function (del_col, del_row) {
-        this.column += del_col;
-        this.row += del_row;
-        this.x += del_col*world.tile_size;
-        this.y -= del_row*world.tile_size;
+    this.move = function (delta_col, delta_row) {
+        // previous_path will represent the full path up until this point;
+        // begin_path is a single "dot" located at the end of previous_path;
+        // end_path is the path from the beginning of the move to the end.
+        this.column += delta_col;
+        this.row += delta_row;
+        this.x += delta_col*world.tile_size;
+        this.y -= delta_row*world.tile_size;
+
+        this.path_x += delta_col*world.tile_size;
+        this.path_y -= delta_row*world.tile_size;
+
+        this.path_history.push([this.path_x, this.path_y]);
+
+        this.draw_path(500);
         if(this.style === "classic") {
-            this.current.animate({x: this.x, y: this.y}, 500);
+            this.current.animate({x: this.x, y: this.y}, 500).toFront();
         }
         else{
-            this.current.animate({cx: this.x, cy: this.y}, 500);
+            this.current.animate({cx: this.x, cy: this.y}, 500).toFront();
         }
     };
+
+    this.draw_path = function (delay) {
+        var x1, x2, y1, y2, length_, begin_path, end_path, previous_path, i;
+        length_ = this.path_history.length;
+
+        previous_path = "M" + this.path_history[0][0] + " " + this.path_history[0][1];
+        for(i = 1; i < this.path_history.length-1; i++){
+            previous_path += "L" + this.path_history[i][0] + " " + this.path_history[i][1];
+        }
+        if(this.trace !== undefined){
+            this.trace.remove();
+        }
+        if(this.end_trace !== undefined){
+            this.end_trace.remove();
+        }
+        this.trace = world.paper.path(previous_path);
+        x1 = this.path_history[length_-2][0];
+        y1 = this.path_history[length_-2][1];
+        x2 = this.path_history[length_-1][0];
+        y2 = this.path_history[length_-1][1]
+        begin_path = "M" + x1 + " " + y1 + "L" + x1 + " " + y1;
+        end_path = {path: "M" + x1 + " " + y1 + "L" + x2 + " " + y2};
+
+        this.end_trace = world.paper.path(begin_path);
+        this.end_trace.animate(end_path, delay);
+    }
 
     this.turn_left = function () {
         this.angle -= 90;
         switch(this.current_orientation) {
             case "east":
                 this.current_orientation = "north";
+                this.path_y -= 6;
                 break;
             case "north":
                 this.current_orientation = "west";
+                this.path_x -= 6;
                 break;
             case "west":
                 this.current_orientation = "south";
+                this.path_y += 6;
                 break;
             case "south":
                 this.current_orientation = "east";
+                this.path_x += 6;
                 break;
         }
+        this.path_history.push([this.path_x, this.path_y]);
+        this.draw_path(500);
+
         if(this.style === "classic") {
             this.current.hide();
-            this.current = this.robot[this.current_orientation].attr({x: this.x, y: this.y}).show();
+            this.current = this.robot[this.current_orientation].attr({x: this.x, y: this.y}).show().toFront();
         }
         else{
             // update the position in case we are in the middle of an animation
             this.current.attr({cx: this.x, cy: this.y});
-            this.current.animate({rotation: this.angle}, 150);
+            this.current.animate({rotation: this.angle}, 150).toFront();
         }
     };
 
 }
-
 
 function VisibleWorld() {
     this.margin_top = 40;
@@ -176,20 +225,13 @@ function VisibleWorld() {
                     background: "white"
     };
 
-    this.init = function (rows, columns) {
+    // figures out dimensions to know how big a world is going to be
+    // so that the "canvas" can be drawn with the appropriate dimensions.
+    this.init = function (columns, rows) {
         var row;
-        if(rows !== undefined) {
-            this.rows = rows;
-        }
-        else{
-            this.rows = 10;
-        }
-        if(columns !== undefined) {
-            this.columns = columns;
-        }
-        else{
-            this.columns = 10;
-        }
+        this.rows = rows;
+        this.columns = columns;
+
         this.world_width = this.columns * this.tile_size + this.margin_left + this.margin_right + this.narrow;
         this.world_height = this.rows * this.tile_size + this.margin_bottom + this.margin_top;
 
@@ -309,7 +351,7 @@ function VisibleWorld() {
         for(row=0; row < this.rows; row++) {
             _y = row*this.tile_size + this.tile_size/2 + this.margin_top + this.narrow/2;
             this.paper.path("M" + start_x +" "+ _y + "L" + end_x + " " + _y).attr({stroke: "#666", "stroke-dasharray": ". "});
-            this.paper.text(this.margin_left_text, _y, row+1);
+            this.paper.text(this.margin_left_text, _y, this.rows-row);
             }
         for(col=0; col < this.columns; col++) {
             _x = col*this.tile_size + this.tile_size/2 + this.margin_left + this.narrow/2;
@@ -318,8 +360,8 @@ function VisibleWorld() {
         }
     };
 
-    this.render = function () {
-        var row, col;
+    this.render = function (saved_world) {
+        var row, col, point;
         if(this.controls === undefined) {
             this.controls = new Controls(this);
         }
@@ -332,37 +374,60 @@ function VisibleWorld() {
         this.create_background();
         this.create_borders();
         this.create_grid();
+        this.restore(saved_world);
         if(this.robot === undefined) {
             this.robot = new VisibleRobot(this);
             this.robot.teleport(1, 1);
         }
     };
 
+    this.restore = function (saved_world) {
+        var row, col, point;
+        if(saved_world.east_walls !== undefined){
+            for(point=0; point< saved_world.east_walls.length; point++){
+                column = saved_world.east_walls[point][0];
+                row = saved_world.east_walls[point][1];
+                this.east_walls[row][column].active = true;
+                this.east_walls[row][column].attr({fill: this.colours.active, stroke: this.colours.border_active});
+                this.east_walls[row][column].toFront();
+            }
+        }
+        if(saved_world.north_walls !== undefined){
+            for(point=0; point< saved_world.north_walls.length; point++){
+                column = saved_world.north_walls[point][0];
+                row = saved_world.north_walls[point][1];
+                this.north_walls[row][column].active = true;
+                this.north_walls[row][column].attr({fill: this.colours.active, stroke: this.colours.border_active});
+                this.north_walls[row][column].toFront();
+            }
+        }
+    };
+
     this.robot_move = function () {
-        var del_row, del_col, new_row, new_col;
+        var delta_row, delta_col, new_row, new_col;
         switch(this.robot.current_orientation) {
             case "east":
-                del_row = 0;
-                del_col = 1;
+                delta_row = 0;
+                delta_col = 1;
                 break;
             case "north":
-                del_row = 1;
-                del_col = 0;
+                delta_row = 1;
+                delta_col = 0;
                 break;
             case "west":
-                del_row = 0;
-                del_col = -1;
+                delta_row = 0;
+                delta_col = -1;
                 break;
             case "south":
-                del_row = -1;
-                del_col = 0;
+                delta_row = -1;
+                delta_col = 0;
                 break;
         }
-        new_row = this.robot.row + del_row;
-        new_col = this.robot.column + del_col;
-        if( new_row > 1 && new_row <= this.rows+1 &&
-           new_col > 1 && new_col <= this.columns+1) {
-            this.robot.move(del_col, del_row);
+        new_row = this.robot.row + delta_row;
+        new_col = this.robot.column + delta_col;
+        if( new_row >= 1 && new_row <= this.rows &&
+           new_col >= 1 && new_col <= this.columns) {
+            this.robot.move(delta_col, delta_row);
         }
     };
 
@@ -385,23 +450,62 @@ function VisibleWorld() {
             "rows": this.rows,
             "columns": this.columns,
             "robot": [this.robot.column, this.robot.row, this.robot.current_orientation, this.robot.style],
-            "walls": this.active_walls,
             "east_walls": active_east_walls,
             "north_walls": active_north_walls
         };
-        j = JSON.stringify(saved_world, "\n");
-        popup = window.open("");
+        j = JSON.stringify(saved_world);
+        popup = window.open("", "World Parameters", "width=320, height=210, scrollbars=yes");
         popup.document.write(j);
-        popup.document.close()
+        popup.document.close();
     };
 
 }
 
 
 $(window).load(function () {
+
+    var saved_world = '{"rows":8,"columns":12,\n"robot":[1,1,"north","ellipse"],"east_walls":[[5,3], [4,4]],"north_walls":[[5,3]]}';
+    try{
+        saved_world = JSON.parse(saved_world);
+    }
+    catch (e){
+        alert("Problem with saved world; will revert to defaults.");
+        saved_world = {"rows":10,
+                        "columns":10,
+                        "robot":[1,1,"east","classic"]};
+    }
     var visible_world = new VisibleWorld();
-    visible_world.init(2, 2);
+    visible_world.init(saved_world.columns, saved_world.rows);
     visible_world.paper = Raphael("World", visible_world.world_width, visible_world.world_height);
-    visible_world.render();
+    visible_world.render(saved_world);
+
+
+    function activate_keys(e) {
+            var keyCode = e.keyCode || e.which,
+            arrow = {left: 37, up: 38, right: 39, down: 40 };
+            switch (keyCode) {
+                case arrow.left:
+                    visible_world.robot.turn_left();
+                break;
+                case arrow.up:
+                    visible_world.robot_move();
+                break;
+            };
+    }
+
+    $("#World").hover(
+      function () {
+        //entering
+        visible_world.background.attr({stroke: "blue", "stroke-width": 5});
+        $(document).bind('keydown', activate_keys);
+      },
+      function () {
+        // leaving
+        visible_world.background.attr({stroke: "black", "stroke-width": 2});
+        $(document).unbind('keydown', activate_keys);
+      }
+    );
+
+
 
 });
